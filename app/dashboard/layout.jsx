266df07197a8b60cd4ff4f05af14e999e5d01244
mustafauzunal <mustafauznl@icloud.com>
2024-11-25 00:48:46 +0300

    Admin Dashboard Next.js app created, sidebar and navbar designed

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,19 @@
+import NavBar from "../ui/dashboard/navbar/navbar";
+import SideBar from "../ui/dashboard/sidebar/sidebar";
+import styles from "../ui/dashboard/dashboard.module.css";
+
+const Layout = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <SideBar></SideBar>
+      </div>
+      <div className={styles.content}>
+        <NavBar></NavBar>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
